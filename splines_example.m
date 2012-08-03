8147f48cfbf497a21869e20990532fa1df388e93
Will Robertson <wspr81@gmail.com>
2012-08-03 16:17:17 +0930

    add shoe example

--- splines_example.m
+++ splines_example.m
@@ -1,3 +1,4 @@
+%%
 
 hfig = figure(1);
 set(hfig,'color',[1 1 1],'name','Spline example');
@@ -7,13 +8,23 @@ clf;
 points = {...
  [0 0],[1 1],[2,0],...
 };
-
 hobbysplines(points,'debug',true,'cycle',true);
-
 
 points = {[1 0],[1 1],[0 1],[0 0]};
 hobbysplines(points,'debug',true,'cycle',true);
 
+axis equal
+axis off
+
+%%
+
+hfig = figure(2); clf;
+set(hfig,'color',[1 1 1],'name','Spline example');
+
+points = {{[0 0] '' 1 1},[0.7 0.8],[0.8 2],[1 4],[0 5],[-1 3.5],[-0.8 2],[-0.8 1]};
+NN = numel(points);
+hobbysplines(points,'debug',true,'defaultTension',2,'cycle',true);
 
 axis equal
 axis off
+
